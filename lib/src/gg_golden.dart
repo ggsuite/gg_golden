70271f3ec8b9bf83c775ba2cad7e892c157325a7
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2025-05-19 12:47:04 +0200

    Initial implementation

--- lib/src/gg_golden.dart
+++ lib/src/gg_golden.dart
@@ -4,11 +4,57 @@
 // Use of this source code is governed by terms that can be
 // found in the LICENSE file in the root of this package.
 
-/// The main class of the package
-class GgGolden {
-  /// Constructor
-  const GgGolden();
+import 'dart:convert';
+import 'dart:io';
+import 'package:collection/collection.dart';
+import 'package:path/path.dart' as p;
+import 'package:test/test.dart';
 
-  /// A sample method
-  String foo() => 'foo';
+/// If this is set to true, the golden files will be updated.
+bool shouldUpdateGoldens() {
+  return Platform.environment['UPDATE_GOLDENS'] == 'true';
+}
+
+/// Updates the golden file with the given content and compares it.
+Future<void> expectGolden(
+  String fileName,
+  dynamic expected, {
+  bool updateGoldensEnabled = true,
+  bool mockPlatformUpdateGoldens = false,
+}) async {
+  final goldensDir = p.join(Directory.current.path, 'test', 'goldens');
+  final filePath = p.join(goldensDir, fileName);
+  final filePathRelative = p.relative(filePath, from: Directory.current.path);
+
+  // Stringify json
+  final expectedStr = const JsonEncoder.withIndent('  ').convert(expected);
+
+  // Write golden file if update is enabled
+  if (updateGoldensEnabled &&
+      (mockPlatformUpdateGoldens || shouldUpdateGoldens())) {
+    await Directory(p.dirname(filePath)).create(recursive: true);
+    await File(filePath).writeAsString(expectedStr);
+  }
+
+  // Read golden file
+  bool needsGoldenUpdate = true;
+  dynamic golden;
+  try {
+    final goldenStr = await File(filePath).readAsString();
+    golden = jsonDecode(goldenStr);
+    needsGoldenUpdate = false;
+  } catch (_) {
+    needsGoldenUpdate = true;
+  }
+
+  if (updateGoldensEnabled) {
+    needsGoldenUpdate =
+        !const DeepCollectionEquality().equals(expected, golden);
+    if (needsGoldenUpdate) {
+      fail('Run "dart run update_goldens" and review "$filePathRelative".');
+    }
+  } else {
+    expect(needsGoldenUpdate, isFalse);
+    expect(expected, equals(golden));
+  }
 }
--- test/gg_golden_test.dart
+++ test/gg_golden_test.dart
@@ -4,15 +4,121 @@
 // Use of this source code is governed by terms that can be
 // found in the LICENSE file in the root of this package.
 
+import 'dart:io';
+
 import 'package:gg_golden/gg_golden.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('GgGolden()', () {
-    group('foo()', () {
-      test('should return foo', () async {
-        const ggGolden = GgGolden();
-        expect(ggGolden.foo(), 'foo');
+  group('expectGolden(fileName, expected, updateGoldensEnabled)', () {
+    final goldensDir = Directory('test/goldens');
+
+    Future<void> recreateGoldensDir() async {
+      // Delete test/goldens dir when existing
+      if (await goldensDir.exists()) {
+        await goldensDir.delete(recursive: true);
+      }
+      await goldensDir.create(recursive: true);
+    }
+
+    group('with updateGoldens = true', () {
+      test('creates a golden file in goldens()', () async {
+        await recreateGoldensDir();
+
+        // Golden file does not exst
+        final goldenFile = File('test/goldens/test/test.golden.json');
+        expect(await goldenFile.exists(), false);
+
+        // Create golden
+        final json = {'foo': 'true'};
+        await expectGolden(
+          'test/test.golden.json',
+          json,
+          mockPlatformUpdateGoldens: true,
+        );
+
+        // Golden file exists
+        expect(await goldenFile.exists(), true);
+
+        // Next golden file should pass
+        await expectGolden(
+          'test/test.golden.json',
+          json,
+        );
+
+        // Testing against a modified golden should fail
+        var message = '';
+        try {
+          final modifiedJson = {'foo': 'juhu'};
+
+          await expectGolden(
+            'test/test.golden.json',
+            modifiedJson,
+          );
+        } catch (e) {
+          expect(e, isA<TestFailure>());
+          message = (e as TestFailure).message!;
+        }
+
+        expect(
+          message,
+          'Run "dart run update_goldens" and review "test/goldens/test/test.golden.json".',
+        );
+      });
+    });
+
+    group('with updateGoldens = false', () {
+      test('does not update golden file', () async {
+        await recreateGoldensDir();
+
+        // Golden file does not exst
+        final goldenFile = File('test/goldens/test/test.golden.json');
+        expect(await goldenFile.exists(), false);
+
+        // Create golden
+        final json = {'foo': 'true'};
+        await expectGolden(
+          'test/test.golden.json',
+          json,
+          mockPlatformUpdateGoldens: true,
+        );
+
+        // Golden file exists
+        expect(await goldenFile.exists(), true);
+
+        // Next golden file should pass
+        await expectGolden(
+          'test/test.golden.json',
+          json,
+        );
+
+        // Try to update with a modified golden and updateGoldens = false
+        // It fails, because updateGoldens is false
+
+        var message = '';
+        try {
+          final modifiedJson = {'foo': 'juhu'};
+
+          await expectGolden(
+            'test/test.golden.json',
+            modifiedJson,
+            updateGoldensEnabled: false,
+            mockPlatformUpdateGoldens: true,
+          );
+        } catch (e) {
+          expect(e, isA<TestFailure>());
+          message = (e as TestFailure).message!;
+        }
+
+        expect(
+          message.split('\n'),
+          [
+            "Expected: {'foo': 'true'}",
+            "  Actual: {'foo': 'juhu'}",
+            "   Which: at location ['foo'] is 'juhu' instead of 'true'",
+            '',
+          ],
+        );
       });
     });
   });
